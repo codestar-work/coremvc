532abef1ff1f0b816159b24adb19614a82e05b34
Kookiat Suetrong <kookiatsuetrong@gmail.com>
2016-07-31 08:59:35 +0700

    update

--- Program.cs
+++ Program.cs
@@ -1,26 +1,29 @@
 using System.IO;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.StaticFiles;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 public class Program {
-
 	public static void Main(string[] args) {
 		var host = new WebHostBuilder()
 			.UseContentRoot(Directory.GetCurrentDirectory())
-			.UseStartup<Program>()
+			.UseUrls("http://+:2000")
+			.UseStartup<Startup>()
 			.UseKestrel()
 			.Build();
-
 		host.Run();
 	}
+}
 
+class Startup {
 	public void ConfigureServices(IServiceCollection services) {
 		services.AddMvc();
 	}
 
 	public void Configure(IApplicationBuilder app) {
+		app.UseStaticFiles();
 		app.UseMvcWithDefaultRoute();
 	}
 }
